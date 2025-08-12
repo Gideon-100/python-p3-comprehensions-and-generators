220fcc2d4880a95547194fe052b29033b07062e3
Gideon lenkai <gideonlenkai2021@gmail.com>
2025-08-12 05:16:39 +0300

    comprehensions and generators

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    """Return a list of even numbers from the given list."""
+    return [n for n in num_list if n % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    """Return a list of sentences with an exclamation mark at the end."""
+    return [sentence + "!" for sentence in sentences]
